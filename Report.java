7e44a657640b22f901d04dd9b60cc38d93713e62
Philip Lam <Philam18@users.noreply.github.com>
2017-04-09 17:20:06 -0700

    Report.java update
    -Completed Functionality
    -Added Documentation
    -createReportCode() no longer requires String[] and Database as parameters
    -Removed deleteReport() so the database can handle deleting reports

--- Report.java
+++ Report.java
@@ -1,54 +1,62 @@
+package test;
 /**
  * A Report is created and submitted to the CYPRESS system by users (citizens)
  * and contains information regarding location and nature of the complaint
- * @author User
- *
  */
 public class Report {
 
-	String owner;
-	String address;
-	String concern;
-	int reportCode;
+	private String owner;
+	private String address;
+	private String concern;
+	private String reportCode;
 	
 	/**
-	 * Constructs an empty Report
+	 * Constructs a report with given
 	 */
-	public Report(){
-		owner = "";
-		address = "";
-		concern = "";
-		reportCode = 0;
+	public Report(String[] data){
+            concern = data[0];
+            address = data[1];
+            owner = data[2];
+            reportCode = createReportCode();
 	}
 	
 	/**
 	 * Generates a reportCode for the Report
-	 * @param string
-	 * @param database
+         * A report code is generated as follows:
+         *  AAA_BBB
+         * Where,
+         *  AAA represents the hashcode of the concern type, and
+         *  BBB represents the hashcode of the address type, and
+         * 
+         * This method aims to achieve the following:
+         * -Every report has its own code
+         * -Two equal reports have the same code
+         * -Two different reports will not have the same code
 	 * @return true if reportCode was successfully generated
 	 */
-	public boolean createReportCode(String[] string, Database database){
-		
-		return true;
+	private String createReportCode(){
+            String productConcern = Integer.toString(this.concern.hashCode());
+            String productAddress = Integer.toString(this.address.hashCode());
+            return productConcern + "_" + productAddress;
 	}
 	
 	/**
 	 * Edits the Report with the given info
 	 * @param string array containing data fields as indices
+         * 0- contains the concern
+         * 1- contains the address
+         * If any of these values are not given, they will be null
 	 * @return true if edit was successful
 	 */
-	public boolean editReport(String[] string){
-		
-		return true;
-	}
-	
-	/**
-	 * Deletes the Report
-	 * @return true if Report was successfully deleted
-	 */
-	public boolean deleteReport(){
-		
-		return true;
+	public boolean editReport(String[] data){
+            if(data[0] != null){
+                this.concern = data[0];
+            }
+            if(data[1] != null){
+                this.address = data[1];
+            }
+            this.reportCode = createReportCode();
+            return true;
 	}
 	
 	/**
@@ -56,7 +64,7 @@ public class Report {
 	 * @param owner of the report
 	 */
 	void setOwner(String owner){
-		this.owner = owner;
+            this.owner = owner;
 	}
 	
 	/**
@@ -64,7 +72,7 @@ public class Report {
 	 * @param address of the Report
 	 */
 	void setAddress(String address){
-		this.address = address;
+            this.address = address;
 	}
 	
 	/**
@@ -72,7 +80,7 @@ public class Report {
 	 * @param concern of the report
 	 */
 	void setConcern(String concern){
-		this.concern = concern;
+            this.concern = concern;
 	}
 	
 	/**
@@ -80,7 +88,7 @@ public class Report {
 	 * @return owner of the Report
 	 */
 	String getOwner(){
-		return owner;
+            return owner;
 	}
 	
 	/**
@@ -88,7 +96,7 @@ public class Report {
 	 * @return address of the Report
 	 */
 	String getAddress(){
-		return address;
+            return address;
 	}
 	
 	/**
@@ -96,6 +104,14 @@ public class Report {
 	 * @return concern of the Report
 	 */
 	String getConcern(){
-		return concern;
+            return concern;
 	}
+        
+        /**
+         * Gets the report code
+         * @return report code
+         */
+        String getReportID(){
+            return reportCode;
+        }
 }
